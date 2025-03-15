527589ba9c15d0833e2acee3f9c9e9266fe0e6e3
Samuel Trew <samuel.trew@outlook.com>
2025-03-15 00:19:07 +0000

    Base soundboard layout in progress

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import Tile from './lib/Tile.svelte'
+</script>
+
+<main>
+  <div class="grid">
+    {#each { length: 9 }, rank}
+      <Tile />
+    {/each}
+  </div>
+</main>
+
+<style lang="scss">
+  .grid {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: 1fr 1fr 1fr;
+    column-gap: 1rem;
+    row-gap: 1rem;
+  }
+</style>
---
+++ src/lib/Tile.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  let count: number = $state(0)
+  const increment = () => {
+    count += 1
+  }
+</script>
+
+<button onclick={increment} class="tile">
+  count is {count}
+</button>
+
+<style lang="scss">
+  .tile {
+    height: 100%;
+    width: 100%;
+    background-color: lightblue;
+    &:hover {
+      background-color: bisque;
+    }
+  }
+</style>
