b22668be8cf5304bfd38d64373f56905b4728eea
Samuel Trew <samuel.trew@outlook.com>
2025-04-17 23:46:20 +0100

    Added more icons and better styling

---
+++ src/lib/Header.svelte
@@ -0,0 +1,18 @@
+<div class="header">
+   <span class="title">Katy Soundboard</span>
+</div>
+
+<style lang="scss">
+   .header {
+      background-color: hotpink;
+      height: 3rem;
+      display: flex;
+      flex-direction: row;
+      justify-content: flex-start;
+      align-items: center;
+   }
+
+   .title {
+      font-size: 1.5rem;
+   }
+</style>
