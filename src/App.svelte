dc284c87836bef954169592416341c8150ff7b01
Samuel Trew <samuel.trew@outlook.com>
2025-04-17 23:46:14 +0100

    Added more icons and better styling

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
   import Tile from './lib/Tile.svelte'
+  import Header from './lib/Header.svelte';
+
   const tiles = [
     {icon: "scissors.svg", music: "test.mp3"},
     {icon: "hand.svg", music: "test.mp3"},
@@ -7,18 +9,39 @@
     {icon: "wheatus.png", music: "test.mp3"},
     {icon: "boy.svg", music: "test.mp3"},
     {icon: "where.svg", music: "test.mp3"},
+    {icon: "river.svg", music: "test.mp3"},
+    {icon: "365.svg", music: "test.mp3"},
+    {icon: "espresso.svg", music: "test.mp3"},
+    {icon: "snail.svg", music: "test.mp3"},
   ]
 </script>
 
-<main>
-  <div class="grid">
-    {#each tiles as {icon, music}}
-      <Tile icon={icon} music={music} />
-    {/each}
+<main class="main">
+  <Header />
+  <div class="app-body">
+    <div class="grid">
+      {#each tiles as {icon, music}}
+        <Tile icon={icon} music={music} />
+      {/each}
+    </div>
   </div>
 </main>
 
 <style lang="scss">
+  .main {
+    height: 100%;
+    width: 100%;
+  }
+
+  .app-body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    height: 100%;
+    width: 100%;
+  }
+
   .grid {
     display: grid;
     grid-template-columns: 1fr 1fr 1fr;
--- src/lib/Tile.svelte
+++ src/lib/Tile.svelte
@@ -1,49 +1,49 @@
 <script lang="ts">
-  type Props = {
-    icon: string
-    music: string
-  }
+   type Props = {
+      icon: string
+      music: string
+   }
 
 	let {icon, music}: Props = $props();
-  let highlight = $state(false)
+   let highlight = $state(false)
 
-  const play = () => {
-    const audio = new Audio(`/${music}`)
-    audio.onended = () => {
-      highlight = false
-    }
-    audio.onplay = () => {
-      highlight = true
-    }
-    audio.play()
-  }
+   const play = () => {
+      const audio = new Audio(`/${music}`)
+      audio.onended = () => {
+         highlight = false
+      }
+      audio.onplay = () => {
+         highlight = true
+      }
+      audio.play()
+   }
 </script>
 
 <button onclick={play} class={`tile ${highlight ? "highlighted": ""}`}>
-  <img loading="lazy" src={`/${icon}`} alt={icon} width={80} height={80} />
+   <img loading="lazy" src={`/${icon}`} alt={icon} width={80} height={80} />
 </button>
 
 <style lang="scss">
-  .tile {
-    padding: 0;
-    height: 100%;
-    width: 100%;
-    border: 1px solid hotpink;
-    &:hover {
-      background-color: hotpink;
-    }
+   .tile {
+      padding: 0;
+      height: 100%;
+      width: 100%;
+      border: 1px solid hotpink;
+      &:hover {
+         background-color: hotpink;
+      }
 
-    img {
-      border-radius: 0.5rem;
-    }
-  }
+      img {
+         border-radius: 0.5rem;
+      }
+   }
 
-  .highlighted {
-    background-color: red;
-    border: 1px solid red;
+   .highlighted {
+      background-color: red;
+      border: 1px solid red;
 
-    &:hover {
-      background-color: darkred;
-    }
-  }
+      &:hover {
+         background-color: darkred;
+      }
+   }
 </style>
